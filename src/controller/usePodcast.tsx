196cf48d5ff05cdcaca45a157485aa8f4f2de011
Antonio Soto <108065401+am-soto@users.noreply.github.com>
2023-03-30 13:04:57 +0200

    feat: add filter (#11)

--- src/controller/usePodcast.tsx
+++ src/controller/usePodcast.tsx
@@ -3,15 +3,30 @@ import { Podcasts } from '@/domain/use-cases/Podcasts'
 import { useEffect, useState } from 'react'
 
 interface IUsePodcast {
+  filter: string
   podcasts: Podcast[]
+  saveFilter: (newFilter: string) => void
 }
 
 export const usePodcast = (): IUsePodcast => {
+  const [filter, setFilter] = useState('')
   const [podcasts, setPodcasts] = useState<Podcast[]>([])
   const podcastsUseCase = new Podcasts()
 
+  const filterPodcasts = () => {
+    return podcasts.filter(
+      (value) =>
+        value.author.toLocaleLowerCase().includes(filter.toLocaleLowerCase()) ||
+        value.title.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
+    )
+  }
+
   const getPodcasts = async () => {
     await podcastsUseCase.getAll().then((r) => setPodcasts(r))
+  }
+
+  const saveFilter = (newFilter: string) => {
+    setFilter(newFilter)
   }
 
   useEffect(() => {
@@ -19,6 +34,8 @@ export const usePodcast = (): IUsePodcast => {
   }, [])
 
   return {
-    podcasts
+    filter,
+    podcasts: filterPodcasts(),
+    saveFilter
   }
 }
--- src/presentation/pages/podcast-list/index.tsx
+++ src/presentation/pages/podcast-list/index.tsx
@@ -1,13 +1,16 @@
 import { usePodcast } from '@/controller/usePodcast'
+import Filter from '@/presentation/components/filter'
 import PodcastCardList from '@/presentation/components/podcast-card-list'
-import { Link } from 'react-router-dom'
+import { Box } from '@mui/material'
 
 const PodcastListPage = () => {
-  const { podcasts } = usePodcast()
+  const { filter, podcasts, saveFilter } = usePodcast()
 
   return (
     <>
-      Podcast List: <Link to='/podcasts/1'>1</Link>
+      <Box display='flex' justifyContent='flex-end' mb={10}>
+        <Filter quantity={podcasts.length} value={filter} onValueChange={saveFilter} />
+      </Box>
       <PodcastCardList podcasts={podcasts} />
     </>
   )
